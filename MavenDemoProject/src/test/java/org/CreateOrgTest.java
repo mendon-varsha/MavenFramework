02db0c5b8a3a9cc2c20fb6fafd3d6296319f1f47
mendon-varsha <mendonvarsha11@gmail.com>
2025-01-06 15:35:17 +0530

    pushing

--- MavenDemoProject/src/test/java/org/CreateOrgTest.java
+++ MavenDemoProject/src/test/java/org/CreateOrgTest.java
@@ -15,4 +15,10 @@ public void createOrgWithDate()
 {
 	Reporter.log("Org Created with date",true);
 }
+
+@Test
+public void createOrgWithName()
+{
+	Reporter.log("Org created with name",true);
+}
 }
